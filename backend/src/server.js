8e954d264b396f309d1afa90766b595c088a14c7
Digambar <ddjail2004@gmail.com>
2025-03-01 13:35:42 +0530

    added port listen

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,3 +4,6 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+setInterval(() => console.log("Server is still running..."), 60000);
+
